c387c77e08cd80db33f5d11ae73f41cbeed675d1
Vigneshwar <77089276+vigneshwar-24@users.noreply.github.com>
2022-07-08 09:01:14 +0530

    Add files via upload

---
+++ Untitled3.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+import numpy as np
+i=1.5    
+w_o=0.8  
+y=0.5    
+r=0.01   
+def dc_dw(a,y,i):
+  dc_da=2*(a-y)
+  da_dw=i
+  return dc_da*da_dw
+  
+w=[w_o]
+a=[w_o*i]
+for j in range(0,100):
+  a.append(w[-1]*i)
+  w.append(w[-1]-r*dc_dw(a[-1],y,i))
+  if(a[-1]-y)**2<0.001:
+    break
+print(a)
+print(" ")
+print(w)
+
+
+# In[ ]:
+
+
+
+
